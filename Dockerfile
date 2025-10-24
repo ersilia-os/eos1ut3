a24f3af131120990921696c299a576d982406b22
Marina18 <marina@ersilia.io>
2025-10-24 12:11:32 +0200

    updated dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM bentoml/model-server:0.11.0-py310
 MAINTAINER ersilia
 
+RUN pip install numpy==1.26.0
 RUN pip install rdkit==2023.9.2
 RUN pip install datamol==0.12.1
-RUN pip install molfeat==0.9.5
-RUN pip install numpy==1.26.0
+RUN pip install molfeat==0.11.0
+
 
 WORKDIR /repo
 COPY . /repo
---
+++ install.sh
@@ -0,0 +1,4 @@
+/home/marina/anaconda3/envs/eos1ut3/bin/python -m pip install numpy==1.26.0
+/home/marina/anaconda3/envs/eos1ut3/bin/python -m pip install rdkit==2023.9.2
+/home/marina/anaconda3/envs/eos1ut3/bin/python -m pip install datamol==0.12.1
+/home/marina/anaconda3/envs/eos1ut3/bin/python -m pip install molfeat==0.11.0
